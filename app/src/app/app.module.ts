d7f53ed0f7c22fa4dd01a8c0ca72da6bb29bac61
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-07-20 10:28:54 +0300

    Firebase
    Connect app to firebase

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environment/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyC-JAyRIrtj1LNgtUhaJTHO2QCBqLfHeuE",
+  authDomain: "babyrentacar-b0505.firebaseapp.com",
+  databaseURL: "https://babyrentacar-b0505-default-rtdb.firebaseio.com",
+  projectId: "babyrentacar-b0505",
+  storageBucket: "babyrentacar-b0505.appspot.com",
+  messagingSenderId: "472232355864",
+  appId: "1:472232355864:web:b4a2a458493ad432327b6d",
+  measurementId: "G-NFVYTKDSRT"
+    }
+  };
