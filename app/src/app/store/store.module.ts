d587132de9a071f8967a8b83e398379bd7ff64d6
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-08-11 00:08:45 +0300

    Update
    Update

--- app/src/app/store/store.module.ts
+++ app/src/app/store/store.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
 
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,7 +12,7 @@ import { RouterModule } from '@angular/router';
     CartComponent
   ],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, SharedModule
   ]
 })
 export class StoreModule { }
