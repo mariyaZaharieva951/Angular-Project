8a9dca0999863d1f69b26df8a2689c6891abc4d1
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-08-11 15:17:51 +0300

    Update
    Update

--- app/src/environment/db.ts
+++
@@ -1,8 +0,0 @@
-// import { getDatabase, ref, onValue } from "firebase/database";
-
-// const db = getDatabase();
-// const starCountRef = ref(db, 'posts/' + postId + '/starCount');
-// onValue(starCountRef, (snapshot) => {
-//   const data = snapshot.val();
-//   updateStarCount(postElement, data);
-// });
